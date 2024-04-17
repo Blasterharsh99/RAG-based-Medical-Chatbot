2b395df9909dfa16ab1392f9f936f8c58d991748
Blasterharsh99 <aggarwalharshit99@gmail.com>
2024-04-18 00:55:44 +0530

    Configured Setup.py to install local package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Harshit Aggarwal',
+    author_email= 'aggarwalharshit99@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
